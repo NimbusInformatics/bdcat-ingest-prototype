3bb90454bf75627f82c4ee6f628e9ba9d2a62f5a
Ann Van <ann.van@gmail.com>
2020-07-13 19:33:31 -0700

    Update process.py
    reorganized code
    added test mode
    added bucket checks for aws
    added params for chunk size and threads
    added s3_modified_date to output manifest
    added s3 key exists check before uploading

--- process.py
+++ process.py
@@ -27,6 +27,7 @@ def main():
 	parser.add_argument('--tsv', required=True, type=argparse.FileType('r'), help='tsv file')
 	parser.add_argument('--gs', default=False, action='store_true', help='upload to Google Cloud')
 	parser.add_argument('--aws', default=False, action='store_true', help='upload to AWS')
+	parser.add_argument('--test', default=False, action='store_true', help='test mode')
 	parser.add_argument('--threads', default=10, help='number of concurrent threads')
 	parser.add_argument('--chunk-size', default=8 * 1024 * 1024, help='mulipart-chunk-size for uploading')
 
@@ -40,11 +41,8 @@ def main():
 		exit()
 
 	# process file
-	od = OrderedDict() 
-	reader = csv.DictReader(args.tsv, dialect='excel-tab')			
-	for row in reader:
-		manifest_row = process_row(od, row)
-
+	od = OrderedDict()
+	read_and_verify_file(od, args) 
 	checksum_files(od, args.threads)
 	
 	if (args.gs):
@@ -58,12 +56,53 @@ def main():
 	else:
 		manifest_filepath += 'manifest.tsv'
 	create_manifest_file(manifest_filepath, od)
-			
-def process_row(od, row):
+
+def read_and_verify_file(od, args) :
+	reader = csv.DictReader(args.tsv, dialect='excel-tab')
+	all_files_readable = True
+	all_buckets_writeable = True
+	for row in reader:
+		if(process_row(od, row, args.test) == False):
+			all_files_readable = False
+	if (args.gs):
+		if(verify_gs_buckets(od, args.test) == False):
+			all_buckets_writeable = False
+	if (args.aws):
+		if(verify_aws_buckets(od, args.test) == False):
+			all_buckets_writeable = False
+	if (all_files_readable == False or all_buckets_writeable == False):
+		print("Script exiting due to errorrs in ", args.tsv.name)
+		exit()
+	if (args.test):
+		print("Test mode:", args.tsv.name, "valid. Script now exiting.")
+		exit()			
+def process_row(od, row, test_mode):
 	local_file = row['file_local_path']
 	od[local_file] = row
-	assert isfile(local_file) and access(local_file, R_OK), \
-       "File {} doesn't exist or isn't readable".format(local_file)
+	# confirm file exists and is readable
+	if (isfile(local_file) and access(local_file, R_OK)):
+		if (test_mode):
+			print("File is readable:", local_file)			
+		return True
+	else:
+		print("File doesn't exist or isn't readable:", local_file)
+		return False
+       
+def verify_gs_buckets(od, test_mode):
+	return
+	
+def verify_aws_buckets(od, test_mode):
+	aws_buckets = {}
+	iam = boto3.client('iam')
+	sts = boto3.client('sts')
+	arn = sts.get_caller_identity()['Arn']
+	all_buckets_writeable = True
+
+	for key, value in od.items(): 
+		bucket_name = get_bucket_name(value)
+		if (aws_bucket_writeable(bucket_name, iam, arn, aws_buckets, test_mode) == False):
+			all_buckets_writeable = False
+	return all_buckets_writeable
 
 def checksum_files(od, num_threads):
 	# FIXME
@@ -79,6 +118,7 @@ def checksum_files(od, num_threads):
  
 def upload_to_aws(od, threads, chunk_size): 
 	aws_client = boto3.client('s3')
+	transfer_config = boto3.s3.transfer.TransferConfig(multipart_chunksize=chunk_size, max_concurrency=threads, use_threads=True)  
 
     #TODO confirm all files with unique names???
     #TODO confirm readable cloud dir ??
@@ -87,39 +127,103 @@ def upload_to_aws(od, threads, chunk_size):
 	for key, value in od.items(): 
 		bucket_name = get_bucket_name(value)
 		s3_file = basename(key)
-		print('attempting to upload ', s3_file, ' to s3://', bucket_name, ' with threads=', threads, ' and chunk_size=', chunk_size)
+		print('attempting to upload ', s3_file, ' to s3://', bucket_name, ' with threads=', threads, ' and chunk_size=', chunk_size, sep='')
  		
-		transfer_config = boto3.s3.transfer.TransferConfig(multipart_chunksize=chunk_size, max_concurrency=threads, use_threads=True)  
-	   # Upload the file
-		try:
-			start = datetime.datetime.now()			
-			computed_checksum = calculate_s3_etag(key, chunk_size)
-			end = datetime.datetime.now()
-			print('elapsed time for checksum:', end - start)
-
-			start = datetime.datetime.now()
-			aws_client.upload_file(key, bucket_name, s3_file, Config=transfer_config)
-			end = datetime.datetime.now()
-			print('elapsed time for aws upload:', end - start)
+		start = datetime.datetime.now()			
+		computed_checksum = calculate_s3_etag(key, chunk_size)
+		end = datetime.datetime.now()
+		print('elapsed time for checksum:', end - start)
+		value['s3_md5sum'] = computed_checksum
+		
+		if (aws_key_exists(aws_client, bucket_name, key)):
+			print("Already exists. Skipping", key)
 			response = aws_client.head_object(Bucket=bucket_name, Key=s3_file)
-			file_size = response['ContentLength']
-			print ("size:", file_size)
-			s3_path = 's3://' + bucket_name + '/' + s3_file
-			md5sum = response['ETag'][1:-1]
-			print('checksum check:', computed_checksum, ':', md5sum)
-			if (computed_checksum == md5sum):
-				print('same checksum')
+			add_aws_manifest_metadata(value, response, 's3://' + bucket_name + '/' + s3_file)
+		else:
+			try:
+				start = datetime.datetime.now()
+				aws_client.upload_file(key, bucket_name, s3_file, Config=transfer_config)
+				end = datetime.datetime.now()
+				print('elapsed time for aws upload:', end - start)
+				response = aws_client.head_object(Bucket=bucket_name, Key=s3_file)
+				add_aws_manifest_metadata(value, response, 's3://' + bucket_name + '/' + s3_file)
+			except ClientError as e:
+				logging.error(e)
+				print(e)
+				value['s3_path'] = ''
+				value['s3_modified_date'] = ''	 
+				value['file_size'] = -1
+
+def aws_bucket_writeable(bucket_name, iam, arn, aws_buckets, test_mode):
+	if (bucket_name in aws_buckets):
+		if (aws_buckets[bucket_name] == 1):
+			return True
+		else:
+			return False
+	else:
+		# Create an arn representing the objects in a bucket
+		bucket_objects_arn = 'arn:aws:s3:::%s/*' % bucket_name
+
+		try:
+			s3 = boto3.resource('s3')
+			if (not(s3.Bucket(bucket_name) in s3.buckets.all())):
+				print('ERROR: bucket does not exist -', bucket_name)
+				return False
+				
+			# Run the policy simulation for the PUT
+			results = iam.simulate_principal_policy(
+				PolicySourceArn=arn,
+				ResourceArns=[bucket_objects_arn],
+				ActionNames=['s3:PutObject']
+			)
+#			print(results)
+			if (results['EvaluationResults'][0]['EvalDecision'] == 'allowed'):
+				aws_buckets[bucket_name] = 1
+				if (test_mode):
+					print("Bucket is writeable: s3://", bucket_name, sep='')			
+				return True
 			else:
-				print('different checksum') 
-			value['s3_path'] = s3_path
-			value['file_size'] = file_size
-			value['md5sum'] = md5sum
-		except ClientError as e:
-			logging.error(e)
-			print(e)
-			value['s3_path'] = ''
-			value['file_size'] = -1
-			value['md5sum'] = ''
+				aws_buckets[bucket_name] = 0
+				print('ERROR: bucket does not exist or is not writeable -', bucket_name)
+				return False
+		except botocore.errorfactory.NoSuchBucket as e:
+			aws_buckets[bucket_name] = 0
+			print('ERROR: bucket does not exist or is not writeable -', bucket_name)
+			return False
+		
+
+def gs_bucket_writeable(bucket_name, test_mode):
+	if (aws_buckets[bucket_name] == 1):
+		return true
+	elif (aws_buckets[bucket_name] == 0):
+		return false
+	else:
+		# FIXME
+		return True
+
+
+def aws_key_exists(aws_client, bucket_name, key):
+	s3 = boto3.resource('s3')
+	bucket = s3.Bucket(bucket_name)
+	objs = list(bucket.objects.filter(Prefix=key))
+	if len(objs) > 0 and objs[0].key == key:
+		return True
+	else:
+		return False
+
+def add_aws_manifest_metadata(fields, response, path):
+	print(response)
+	file_size = response['ContentLength']
+	print ("size:", file_size)
+	md5sum = response['ETag'][1:-1]
+	print('checksum check:', fields['s3_md5sum'], ':', md5sum)
+	if (fields['s3_md5sum'] == md5sum):
+		print('same checksum')
+	else:
+		print('different checksum')
+	fields['s3_path'] = path
+	fields['s3_modified_date'] = response['ResponseMetadata']['HTTPHeaders']['last-modified'] 
+	fields['s3_file_size'] = file_size
 
 # code taken from https://stackoverflow.com/questions/12186993/what-is-the-algorithm-to-compute-the-amazon-s3-etag-for-a-file-larger-than-5gb#answer-19896823
 # more discussion of how checksum is calculated for s3 here: https://stackoverflow.com/questions/6591047/etag-definition-changed-in-amazon-s3/28877788#28877788
